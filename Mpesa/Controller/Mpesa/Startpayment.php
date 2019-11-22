8b865ad1c9077344ccc1f810d61d7115edae80b4
BransonGitomeh <sirbranson67@gmail.com>
2019-11-22 13:18:34 +0300

    fix: format code for readability

--- Mpesa/Controller/Mpesa/Startpayment.php
+++ Mpesa/Controller/Mpesa/Startpayment.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: user
@@ -20,8 +21,7 @@ class Startpayment extends \Magento\Framework\App\Action\Action
         \Safaricom\Mpesa\Helper\Data $mpesahelper,
         \Safaricom\Mpesa\Model\Stkpush $stkpush,
         array $data = []
-    )
-    {
+    ) {
         $this->_stkpush = $stkpush;
         $this->_mpesa = $mpesa;
         $this->cart = $cart;
@@ -31,74 +31,104 @@ class Startpayment extends \Magento\Framework\App\Action\Action
     }
 
 
-public function execute()
+    public function execute()
     {
         $phone = $this->getRequest()->getParam('phone');
 
-        $this->_logger->debug('debug1234'); 
-        
-       // $phone = '254720108418';
-       if(preg_match("/(\+?254|0|^){1}[-. ]?[7]{1}([0-2]{1}[0-9]{1}|[9]{1}[0-2]{1})[0-9]{6}\z/", $phone)) {
-        $token = $this->_mpesahelper->generateToken();
-        $live  = $this->_mpesahelper->getGeneralConfig('live_or_dev');
-        $url   = ($live == 'Yes' ? 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest' : 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
+        $this->_logger->debug('debug1234');
 
-        // $url   = $this->_mpesahelper->getGeneralConfig('mpesa_request_url');
-        $curl  = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$token)); //setting custom header
+        // $phone = '254720108418';
+        if (preg_match("/(\+?254|0|^){1}[-. ]?[7]{1}([0-2]{1}[0-9]{1}|[9]{1}[0-2]{1})[0-9]{6}\z/", $phone)) {
+            $token = $this->_mpesahelper->generateToken();
+            $live  = $this->_mpesahelper->getGeneralConfig('live_or_dev');
+            $url   = ($live == 'Yes'
+                ? 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
+                : 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
 
-        $passkey = $this->_mpesahelper->getGeneralConfig('passkey');
-        $paybill = $this->_mpesahelper->getGeneralConfig('my_paybill');
+            // $url   = $this->_mpesahelper->getGeneralConfig('mpesa_request_url');
+            $curl  = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt(
+                $curl,
+                CURLOPT_HTTPHEADER,
+                array(
+                    'Content-Type:application/json', 'Authorization:Bearer ' . $token
+                )
+            ); //setting custom header
 
-
-        date_default_timezone_set('Africa/Nairobi');
-        $date = new \DateTime('now');
-        $date->setTimezone(new \DateTimeZone('UTC'));
-        $str_server_now = $date->format('YmdHis');
-        date_default_timezone_set('UTC');
-        $timestamp =  $str_server_now;
-
-        $amount = $this->cart->getQuote()->getGrandTotal();
-        $account_id = $this->cart->getQuote()->getId();
-        $customerId = $this->cart->getQuote()->getCustomer()->getId();
+            $passkey = $this->_mpesahelper->getGeneralConfig('passkey');
+            $paybill = $this->_mpesahelper->getGeneralConfig('my_paybill');
 
 
-        $curl_post_data = array(
-            //Fill in the request parameters with valid values
-            'BusinessShortCode' => $paybill,
-            'Password' => base64_encode($paybill.$passkey.$timestamp),
-            'Timestamp' => $timestamp,
-            'TransactionType' => 'CustomerPayBillOnline',
-            'Amount' => number_format($amount,0, '.', ''),
-            'PartyA' =>  $this->_mpesahelper->formatPhone($phone),
-            'PartyB' => $paybill,
-            'PhoneNumber' => $this->_mpesahelper->formatPhone($phone),
-            'CallBackURL' => $this->_url->getUrl('safaricommpesa/mpesa/stkpushlistener'),
-            'AccountReference' => $account_id,
-            'TransactionDesc' => 'Magento Order'
-        );
+            date_default_timezone_set('Africa/Nairobi');
+            $date = new \DateTime('now');
+            $date->setTimezone(new \DateTimeZone('UTC'));
+            $str_server_now = $date->format('YmdHis');
+            date_default_timezone_set('UTC');
+            $timestamp =  $str_server_now;
 
-        $data_string = json_encode($curl_post_data);
+            $amount = $this->cart->getQuote()->getGrandTotal();
+            $account_id = $this->cart->getQuote()->getId();
+            $customerId = $this->cart->getQuote()->getCustomer()->getId();
 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 
-        $curl_response = curl_exec($curl);
+            $curl_post_data = array(
+                //Fill in the request parameters with valid values
+                'BusinessShortCode' => $paybill,
+                'Password' => base64_encode($paybill . $passkey . $timestamp),
+                'Timestamp' => $timestamp,
+                'TransactionType' => 'CustomerPayBillOnline',
+                'Amount' => number_format($amount, 0, '.', ''),
+                'PartyA' =>  $this->_mpesahelper->formatPhone($phone),
+                'PartyB' => $paybill,
+                'PhoneNumber' => $this->_mpesahelper->formatPhone($phone),
+                'CallBackURL' => $this->_url->getUrl('safaricommpesa/mpesa/stkpushlistener'),
+                'AccountReference' => $account_id,
+                'TransactionDesc' => 'Magento Order'
+            );
 
-        $json = json_decode($curl_response,true);
-        if(isset($json['errorCode']))
-        {
-            echo json_encode(['success'=>false,url=>$url,'message'=>$json['errorMessage'],live=>$live]);
+            $data_string = json_encode($curl_post_data);
+
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+            $curl_response = curl_exec($curl);
+
+            $json = json_decode($curl_response, true);
+            if (isset($json['errorCode'])) {
+                echo json_encode([
+                    'success' => false,
+                    url => $url,
+                    'message' => $json['errorMessage'],
+                    live => $live
+                ]);
+            } elseif (isset($json['ResponseCode'])) {
+                //return json_encode(['success'=>true,'message'=>$json['ResponseDescription']]);
+                $this->_stkpush->setData([
+                    'account_id' => $account_id,
+                    'merchant_request_id' => $json['MerchantRequestID'],
+                    'checkout_request_id' => $json['CheckoutRequestID'],
+                    'phone' => $phone,
+                    'customer_id' => $customerId
+                ])->save();
+
+                echo json_encode([
+                    live => $live,
+                    url => $url,
+                    'success' => true,
+                    'message' => $json['CustomerMessage'],
+                    'm_id' => $json['MerchantRequestID'],
+                    'c_id' => $json['CheckoutRequestID']
+                ]);
+            }
+        } else {
+            echo json_encode([
+                live => $live,
+                url => $url,
+                'success' => false,
+                'message' => "Phone number is not a safaricom number"
+            ]);
         }
-        elseif(isset($json['ResponseCode'])){
-            //return json_encode(['success'=>true,'message'=>$json['ResponseDescription']]);
-            $this->_stkpush->setData(['account_id'=>$account_id,'merchant_request_id'=>$json['MerchantRequestID'],'checkout_request_id'=>$json['CheckoutRequestID'],'phone'=>$phone,'customer_id'=>$customerId])->save();
-            echo json_encode([live=>$live,url=>$url,'success'=>true,'message'=>$json['CustomerMessage'],'m_id'=>$json['MerchantRequestID'],'c_id' =>$json['CheckoutRequestID']]);
-        }
-    } else {
-        echo json_encode([live=>$live,url=>$url,'success'=>false,'message'=>"Phone number is not a safaricom number"]);
     }
-}
 }
