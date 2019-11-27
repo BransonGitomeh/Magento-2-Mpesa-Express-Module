f61f0889fa67341cab37ea4bafa01f3220bf8c10
Makasi Kali <jessy3g@gmail.com>
2019-11-27 13:14:17 +0300

    Update Startpayment.php

--- Mpesa/Controller/Mpesa/Startpayment.php
+++ Mpesa/Controller/Mpesa/Startpayment.php
@@ -67,8 +67,7 @@ class Startpayment extends \Magento\Framework\App\Action\Action
             'PartyA' =>  $this->_mpesahelper->formatPhone($phone),
             'PartyB' => $paybill,
             'PhoneNumber' => $this->_mpesahelper->formatPhone($phone),
-            'CallBackURL' => $this->_url->getUrl('safaricommpesa/mpesa/stkpushlistener');
-            //'http://e99ade92.ngrok.io/lipaCallback/magento-push',
+            'CallBackURL' => $this->_url->getUrl('safaricommpesa/mpesa/stkpushlistener'),
             'AccountReference' => $account_id,
             'TransactionDesc' => 'Magento Order'
         );
