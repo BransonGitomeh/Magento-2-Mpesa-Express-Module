24a58007a2bb7ada7ff40423f6a6ddb8199482bb
Makasi Kali <jessy3g@gmail.com>
2019-12-03 12:04:54 +0300

    Change: Callback URL to Controller Action

--- Mpesa/Controller/Mpesa/Startpayment.php
+++ Mpesa/Controller/Mpesa/Startpayment.php
@@ -67,7 +67,8 @@ class Startpayment extends \Magento\Framework\App\Action\Action
             'PartyA' =>  $this->_mpesahelper->formatPhone($phone),
             'PartyB' => $paybill,
             'PhoneNumber' => $this->_mpesahelper->formatPhone($phone),
-            'CallBackURL' => 'https://domey-magento-bridge.herokuapp.com/lipaCallback/magento-push',
+            'CallBackURL'=>$this->_url->getUrl('safaricommpesa/mpesa/stkpushlistener'),
+            //'CallBackURL' => 'https://domey-magento-bridge.herokuapp.com/lipaCallback/magento-push',
             'AccountReference' => $account_id,
             'TransactionDesc' => 'Magento Order'
         );
